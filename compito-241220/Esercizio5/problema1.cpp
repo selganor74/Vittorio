9530a16e28d8c69ce89fa552129a1dbb63ee44be
Gabriele Rossetti <selganor74@gmail.com>
2025-01-03 22:41:33 +0100

    Commenti su problema1

--- compito-241220/Esercizio5/problema1.cpp
+++ compito-241220/Esercizio5/problema1.cpp
@@ -6,43 +6,72 @@ const int da_indovinare = 13;
 
 int main()
 {
+    // breve messaggio "introduttivo"
     cout << "Ho pensato ad un numero tra 1 e 20 (estremi compresi)..." << endl
-         << "Prova ad indovinarlo !" << endl;
+         << "Prova ad indovinarlo !" << endl
+         << endl;
 
+    // conterrà il tentativo fatto dall'utente
     int tentativo = 0;
+
+    // non strettamente richiesto dal problema. Serve a contare i tentativi fatti.
     int num_tentativi = 0;
-    do
+
+    // il loop è "infinito"! Si potrà uscire (con un break) indovinando o inserendo 0. Oppure premendo CTRL+C !
+    while(true)
     {
+        // incrementiamo il numero di tentativi fatti.
         num_tentativi++;
-        cout << "Fai il tuo tentativo [0 per smettere di giocare]: ";
+
+        // stampa un messaggio per richiedere un nuovo tentativo.
+        cout << "Fai il tuo tentativo [inserisci 0 o premi CTRL+C per smettere di giocare]: ";
+
+        // legge il tentativo fatto dall'utente
         cin >> tentativo;
 
+        // serve solo a fare un po' di spazio
+        cout << endl;
+
+        // se l'utente ha inserito 0 ...
         if (tentativo == 0)
         {
-            cout << endl << "Sarà per la prossima volta! Ciao!" << endl;
+            // stampiamo un messaggio ed interrompiamo il loop
+            cout << "Sarà per la prossima volta!" << endl
+                 << endl
+                 << "Ciao!" << endl
+                 << endl;
+
             break;
         }
 
+        // se l'utente ha indovinato ...
         if (tentativo == da_indovinare)
         {
+            // stampiamo un messaggio ed interrompiamo il loop
             cout << "Bravo, hai indovinato in " << num_tentativi << " tentativo/i" << endl;
             break;
         }
         else
         {
+            // l'utente non ha indovinato ...
             cout << "No, non hai indovinato! ";
+            
             if (tentativo < da_indovinare)
             {
+                // ... inserendo un valore troppo basso
                 cout << "Il tuo tentativo è troppo basso!";
             }
             else
             {
+                // ... inserendo un valore troppo alto
                 cout << "Il tuo tentativo è troppo alto!";
             }
+
+            // ritorno a capo per fare un po' di spazio nell'output
             cout << endl
                  << endl;
         }
-    } while (true);
+    } // while
 
     return 0;
 }
