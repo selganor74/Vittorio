cb049825498529f7232b859c1946c46fd71df94d
Gabriele Rossetti <selganor74@gmail.com>
2025-01-04 08:19:49 +0100

    Esercizio 1 punto2

---
+++ compito-241220/Esercizio1/punto2.cpp
@@ -0,0 +1,64 @@
+/*
+
+    L'algoritmo per questo programma:
+
+    1 Visualizza un messaggio di benvenuto
+    2 Richiedi il valore dell'estremo A
+    3 Richiedi il valore dell'estremo B assicurandoti che sia maggiore o uguale ad A
+    4 per ogni valore n compreso tra B ed A
+    5   Visualizza n
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+// gli estremi dell'intervallo
+int A;
+int B;
+
+int main()
+{
+    // un messaggio di "benvenuto" che spieghi all'utente cosa fa il programma
+    cout << "Visualizza tutti i numeri interi un intervallo [A, B] dove A e B possono essere positivi e/o negativi" << endl
+         << endl;
+
+    // richiesta dell'estremo A
+    cout << "Inserisci l'estremo A: ";
+    cin >> A;
+
+    while (true)
+    {
+        // richiesta dell'estremo B
+        cout << "Inserisci l'estremo B [ >= " << A << " ]: ";
+        cin >> B;
+
+        // verifica che l'estremo B sia maggiore o uguale ad A ...
+        if (B >= A)
+        {
+            // ... ok possiamo uscire dal ciclo while
+            break;
+        }
+
+        // altrimenti visualizziamo un errore e richiediamo l'input
+        cout << endl
+             << "L'estremo B deve essere maggiore o uguale all'estremo A (" << A << ")." << endl
+             << endl;
+    }
+
+    cout << endl;
+
+    // ciclo tra gli estremi A e B
+    for (int n = A; n <= B; n++)
+    {
+        cout << n;
+        if (n != B)
+            cout << ", ";
+    }
+
+    cout << endl
+         << endl;
+
+    return 0;
+}
