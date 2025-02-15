fcb7f7ff4b1bd67c97035640943da9dbfacffac7
Gabriele Rossetti <selganor74@gmail.com>
2025-02-15 11:59:13 +0100

    esercizio media

---
+++ esercitazioni/Esercizio Media/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+double numero_primo;
+double numero_secondo;
+double risultato;
+
+cout << "inserisci numero_primo" << endl;
+cin >> numero_primo;
+cout << "inserisci numero_secondo" << endl;
+cin >> numero_secondo;
+
+risultato=(numero_primo+numero_secondo)/2;
+cout << "la media: " << risultato << endl;
+
+
+
+
+
+
+
+
+
+
+
+}
